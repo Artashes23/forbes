dd2a21a8a634a821c5553f67287ac38b5dd4edb2
Artashes <artashes.badalyan.999@gmail.com>
2023-11-22 11:34:03 +0400

    Get Sizes

--- cypress/e2e/forbesAds.cy.js
+++ cypress/e2e/forbesAds.cy.js
@@ -1,27 +1,22 @@
 import mainPage, { ads_url } from "../pages/mainPage";
-import Data, { slots } from "../fixtures/data";
+import Data  from "../fixtures/data";
 
 describe('Check google ads', () => { 
     it('Should have expected data', () => {
         cy.visit('/');
         cy.scrollTo('bottom', { duration: 6000, easing: 'linear' });
-
+        cy.pause()
         cy.window().then((win) => {
             if (win.fbsads) {
                 const adSlots = win.fbsads.adSlots.keys();
                 const slotsArray = Array.from(adSlots);
-                let sizes = [];
+                expect(slotsArray).to.deep.equal(Data.slots)
 
-                expect(slotsArray).to.deep.equal(Data.slots);
-
-                for (let slotPos in slotsArray) {
-                    sizes.push(win.fbsads.getSizes(slotPos));
-                }
-
-                
-                // expect(sizes).to.deep.equal(Data.sizes);
+                let sizes = win.fbsads.getSizes('top')
+                expect(sizes).to.deep.equal(Data.sizes);
 
                 expect(win.fbsads.getZone()).to.equal('home');
+                expect(win.fbsads.getSite()).to.equal('fdc.forbes')
             } else {
                 cy.log('Slots not found');
             }
--- cypress/fixtures/data.js
+++ cypress/fixtures/data.js
@@ -1,29 +1,16 @@
-class Data {
-  slots = [ 'top','topx-1','topx-2','topx-3','ntv-home','ntv-deskchannel-1','ntv-deskchannel-2','ntv-deskchannel-3','ntv-deskchannel-4','ntv-deskchannel-5' ]
-  
+class Data  {
+  slots = ['top', 'topx-1', 'topx-2', 'topx-3', 'ntv-home', 'ntv-deskchannel-1', 'ntv-deskchannel-2', 'ntv-deskchannel-3', 'ntv-deskchannel-4', 'ntv-deskchannel-5']
   sizes = [
-    [
-        300,
-        50
-    ],
-    [
-        320,
-        50
-    ],
-    [
-        7,
-        1
-    ],
-    [
-        7,
-        2
-    ],
-    [
-        1,
-        1
-    ],
+    [728, 90],
+    [970, 66],
+    [970, 250],
+    [970, 90],
+    [7, 1],
+    [7, 2],
+    [1, 1],
     "fluid"
-]
-}
+  ];
+  
+};
 
-module.exports = new Data();
+module.exports = new Data;
