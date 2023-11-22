8060833e864341bc9311a45ac566220d8e56f9b5
Artashes <artashes.badalyan.999@gmail.com>
2023-11-22 14:48:36 +0400

    GetSizes

--- cypress/e2e/forbesAds.cy.js
+++ cypress/e2e/forbesAds.cy.js
@@ -1,22 +1,30 @@
 import mainPage, { ads_url } from "../pages/mainPage";
-import Data  from "../fixtures/data";
+import Data from "../fixtures/data";
 
-describe('Check google ads', () => { 
+describe('Check google ads', () => {
     it('Should have expected data', () => {
         cy.visit('/');
         cy.scrollTo('bottom', { duration: 6000, easing: 'linear' });
-        cy.pause()
+
         cy.window().then((win) => {
             if (win.fbsads) {
                 const adSlots = win.fbsads.adSlots.keys();
                 const slotsArray = Array.from(adSlots);
-                expect(slotsArray).to.deep.equal(Data.slots)
+                const sizesObject = {};
 
-                let sizes = win.fbsads.getSizes('top')
-                expect(sizes).to.deep.equal(Data.sizes);
+                expect(slotsArray).to.deep.equal(Data.slots);
+
+                slotsArray.forEach((slotId) => {
+                    const sizes = win.fbsads.adSlots.get(slotId);
+                    const pos = sizes.getTargetingMap()['pos'][0];
+                    const sizesArray = win.fbsads.getSizes(pos);
+                    sizesObject[pos] = sizesArray;
+                });
+
+                expect(sizesObject).to.deep.equal(Data.sizes);
 
                 expect(win.fbsads.getZone()).to.equal('home');
-                expect(win.fbsads.getSite()).to.equal('fdc.forbes')
+                expect(win.fbsads.getSite()).to.equal('fdc.forbes');
             } else {
                 cy.log('Slots not found');
             }
--- cypress/fixtures/data.js
+++ cypress/fixtures/data.js
@@ -1,15 +1,36 @@
 class Data  {
   slots = ['top', 'topx-1', 'topx-2', 'topx-3', 'ntv-home', 'ntv-deskchannel-1', 'ntv-deskchannel-2', 'ntv-deskchannel-3', 'ntv-deskchannel-4', 'ntv-deskchannel-5']
-  sizes = [
-    [728, 90],
-    [970, 66],
-    [970, 250],
-    [970, 90],
-    [7, 1],
-    [7, 2],
-    [1, 1],
-    "fluid"
-  ];
+  sizes =  {
+    top: [
+      [728, 90],
+      [970, 66],
+      [970, 250],
+      [970, 90],
+      [7, 1],
+      [7, 2],
+      [1, 1],
+      'fluid'
+    ],
+    topx: [
+      [728, 90],
+      [970, 66],
+      [970, 250],
+      [970, 90],
+      [1, 1],
+      'fluid'
+    ],
+    'ntv-home': [
+      [2, 3],
+      [1, 1],
+      'fluid'
+    ],
+    'ntv-deskchannel': [
+      [2, 3],
+      [1, 1],
+      'fluid'
+    ]
+  };
+  
   
 };
 
